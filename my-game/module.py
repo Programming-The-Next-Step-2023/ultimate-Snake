17eb362c065619695d6b628879170d205ed1aaa7
piakoch <123562360+piakoch@users.noreply.github.com>
2023-05-12 10:01:33 +0200

    Create theoretical structure for the game

---
+++ my-game/module.py
@@ -0,0 +1,39 @@
+# import packages
+import turtle
+import random
+import time
+
+
+# create the screen (width, height, title, colour etc)
+
+# create a border
+
+# score
+
+# snake characteristics
+
+# food
+
+# scoring
+
+# define how to move (4 directions)
+
+# keyboard binding (which buttons control movement)
+
+# main loop
+
+    #snake and food collision (update snake length, food position, score, snake speed)
+
+
+    # snake and border collision
+
+
+    # snake collision
+
+    # game over (state final score)
+
+
+
+
+
+
